14fa705594fb55ce3217bcfb8758131956afe0fe
unknown <layard3@gmail.com>
2021-11-22 21:56:02 +0800

    create room save

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # render model Room in
 from .models import Room
@@ -31,5 +31,14 @@ def room(request, pk):
 
 def createRoom(request):
     form = RoomForm()
+    
+    if request.method == 'POST':
+        # this print all the data
+        # print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
