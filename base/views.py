be0873fa16be91c331ccca9149d256f6175bfab2
unknown <layard3@gmail.com>
2021-11-25 22:49:02 +0800

    add many to many field called participants

--- base/views.py
+++ base/views.py
@@ -106,16 +106,18 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     # query .model.message -> get all messages that related to this room
     room_messages = room.message_set.all().order_by('-create')
-
+    participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
             user=request.user,
             room=room,
             body=request.POST.get('body')
         )
+        # add commentor in participants(many to many field)
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 # if not login -> redirect user to login page
